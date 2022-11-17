8f3ca89c7f8d6e7e1be085bebcb0c12f6a9f0621
jjrising <jgrubb@nuvalence.io>
2022-11-16 19:18:15 -0500

    Init Spring project.

---
+++ kafka-semaphore/src/main/java/com/nuvalence/kafka/queueing/KafkaSemaphoreApplication.java
@@ -0,0 +1,13 @@
+package com.nuvalence.kafka.queueing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSemaphoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSemaphoreApplication.class, args);
+	}
+
+}
---
+++ kafka-semaphore/src/test/java/com/nuvalence/kafka/queueing/KafkaSemaphoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvalence.kafka.queueing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSemaphoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
